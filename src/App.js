9ff99433461d9637fd403e2fe2d9d5f07e5ff80f
Ivan Chan <ichan018@ucr.edu>
2023-06-09 14:10:58 -0700

    Initialize git commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import ivan_screenshot from './ivan_screenshot.png';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src={ivan_screenshot} alt="Ivan's face" height={200} width={175}/>
+          <p>Hello world!</p>
+          <p>I am Ivan Chan, an aspiring software engineer.</p>
+          <p>Check out these apps:</p>
+          
+          <p>Check out these links:</p>
+          <a
+            className="App-link"
+            href="https://ivan-s-chan.w3spaces.com/"
+            target="_blank"
+            rel="nooimport logo from './logo.svg';
+  pener noreferrer"
+          >
+            My CS Portfolio
+          </a>
+          
+          <a
+            className="App-link2"
+            href="https://www.linkedin.com/in/ivan-s-chan/"
+            target="_blank"
+            rel="nooimport logo from './logo.svg';
+  pener noreferrer"
+          >
+            Linkedin
+          </a>
+       
       </header>
     </div>
   );
